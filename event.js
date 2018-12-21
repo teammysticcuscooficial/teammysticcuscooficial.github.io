646ec716fa566ea5628254798a9ae8fbc06f54ec
teammysticcuscooficial <43480308+teammysticcuscooficial@users.noreply.github.com>
2018-12-21 16:57:48 -0500

    size

--- event.js
+++ event.js
@@ -223,7 +223,7 @@ jQuery(document).ready(function ($) {
         // registro de conteúdo na caixa de informações
         // do marcador
         var infowindow = new google.maps.InfoWindow({
-            content: '<div class="title-map"><h5>' + title + '</h5></div>' + '<div class="endereco"><input style="border: 0;margin-right: 6px;" id="foo" value="' + address + '" readonly><button id="copy-button" class="" data-clipboard-target="#foo"><img width="12px" src="https://clipboardjs.com/assets/images/clippy.svg" alt="Copy to clipboard" title="Copy to clipboard"></button></div>'
+            content: '<div class="title-map"><h5>' + title + '</h5></div>' + '<div class="endereco"><input style="border: 0;margin-right: 6px;" id="foo" value="' + address + '" readonly><button id="copy-button" class="" data-clipboard-target="#foo"><img width="15px" src="https://clipboardjs.com/assets/images/clippy.svg" alt="Copy to clipboard" title="Copy to clipboard"></button></div>'
         });
 
         // exibe marcador quando clicado
